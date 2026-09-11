d74fc4669631e58d36333190bfe723b2d64c75cc
uxzane6225 <132722108+uxzane6225@users.noreply.github.com>
2026-09-11 20:18:47 +0800

    Added Voice API Feature

--- coffee_fetch_api.py
+++ coffee_fetch_api.py
@@ -1,11 +1,13 @@
 import requests
+import speech_recognition as sr
 
 base_url = "https://api.sampleapis.com/coffee/hot"
+r = sr.Recognizer()
 
 def get_coffee(name):
     url = f"{base_url}?title={name}"
     response = requests.get(url)
-    # print(response)
+    print(response)
     return response
 
 def handle_response(response):
@@ -18,7 +20,52 @@ def look_menu():
     url = base_url
     response = requests.get(url)
     return response
-while True:
+
+
+def getCoffee():
+    isGettingCoffee = True
+    while isGettingCoffee:
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+
+                r.adjust_for_ambient_noise(source, duration=0.2)
+                audio = r.listen(source)
+                text = r.recognize_google(audio)
+                text = text.lower()
+                print("You said:", text)
+
+                f = open('output.txt', 'a')
+                f.write(text)
+                f.write("\n")
+                f.close()
+
+                toWords = text.split()
+                newText = ""
+                for word in toWords:
+                    capitalized = word.capitalize()
+                    newText += f"{capitalized} "
+
+                print(newText)
+
+                if newText:
+                    return newText.strip()
+                elif "exit" in text:
+                    print("Exiting program...")
+                    isGettingCoffee = False
+
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+
+        except KeyboardInterrupt:
+            print("Program terminated by user")
+            isGettingCoffee = False
+
+run = True
+while run:
     try:
         see = input("Look at the menu? (y/n): ")
         if see == "y" or see == "Y":
@@ -36,10 +83,21 @@ while True:
                         print(f"- {ingredient}")
                     print("---------------------------------\n")
             else:
-                break
+                run = False
 
-        coffee = input("Enter a Coffee: ")
-
+        inputType = input("Type or voice? (t/v): ")
+        coffee = ""
+        isInputting = True
+        while isInputting:
+            if inputType == "t" or inputType == "T":
+                coffee = input("Enter a Coffee: ")
+                isInputting = False
+            elif inputType == "v" or inputType == "V":
+                coffee = getCoffee()
+                isInputting = False
+            else:
+                print("again")
+        print("After inputting: " + coffee)
         response = get_coffee(coffee)
         data = handle_response(response)
         # print(data)
@@ -53,10 +111,10 @@ while True:
                 print(f"- {ingredient}")
         else:
             raise Exception("Coffee not found!")
-        break
+        run = False
     except ConnectionError:
         print("No connection was made")
-        break
+        run = False
     finally:
         print("The end")
-        break
+        run = False
